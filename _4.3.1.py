ddfde32a83ae491615f52cc16311e471393afe80
Will Frasher <wfrasher@gmail.com>
2016-03-03 18:42:30 -0500

    code

---
+++ _4.3.1.py
@@ -0,0 +1,13 @@
+﻿# put $1000 in bank and get 3% interest anually  
+
+# use while loop to determine how long it will take for the account to have at least $1200
+
+
+
+account = 0
+year = 0
+while account <= 1200:
+    account = account + 1000 + (account * .03)
+    year += 1
+
+print("$" + str(account) + "   " + "Years: " + str(year)) 
